ad86611b054ba732cac527020d493b2440347f69
sven-pke <sven-perschke@gmx.de>
2023-05-09 15:19:22 +0200

    Korrekturen

--- challenge_01.js
+++ challenge_01.js
@@ -10,10 +10,10 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 */
 
 let eingabe1 = prompt("Erste Zahl eingeben: "); // Eingabe der ersten Zahl. Prompt nimmt alles als String Datentyp.
-let eingabe2 =prompt("Zweite Zahl eingeben: "); // Eingabe der zweiten Zahl. Prompt nimmt alles als String Datentyp.
+let eingabe2 = prompt("Zweite Zahl eingeben: "); // Eingabe der zweiten Zahl. Prompt nimmt alles als String Datentyp.
 let zahl1 = parseInt(eingabe1); // Umwandeln des Datentyps von String -> Ganzzahl Integer/Number
 let zahl2 = parseInt(eingabe2); // Umwandeln des Datentyps von String -> Ganzzahl Integer/Number
-let summe = zahl1 + zahl2 // Addition der zwei Ganzzahlen.
+let summe = zahl1 + zahl2; // Addition der zwei Ganzzahlen.
 
 console.log("Die Summe der Zahlen ist: " + summe); // Ausgabe der Summe als Ganzzahl.
 
