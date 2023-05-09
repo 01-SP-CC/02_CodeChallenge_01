3e857197dd250582b54a1ca97229f543c762c0a1
sven-pke <sven-perschke@gmx.de>
2023-05-09 14:34:12 +0200

    Funktioniert schonmal

--- challenge_01.js
+++ challenge_01.js
@@ -9,3 +9,11 @@ Geben Sie die Summe der Zahlen in die Konsole aus:
 "Die Summe der Zahlen ist: summe"
 */
 
+let eingabe1 = prompt("Erste Zahl eingeben: ");
+let eingabe2 =prompt("Zweite Zahl eingeben: ");
+let zahl1 = parseInt(eingabe1);
+let zahl2 = parseInt(eingabe2);
+let summe = zahl1 + zahl2
+
+console.log(summe);
+
